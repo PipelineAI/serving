32fe051718cabb4b2d6578588e1877bd9b44193e
cfregly <chris@fregly.com>
2018-01-21 01:11:21 +0000

    added build and push scripts, added cpu

---
+++ tensorflow_serving/tools/docker/build.sh
@@ -0,0 +1,2 @@
+docker build -t pipelineai/serving-gpu:1.5.0 -f Dockerfile.pipeline-gpu .
+docker build -t pipelineai/serving-cpu:1.5.0 -f Dockerfile.pipeline-cpu .
---
+++ tensorflow_serving/tools/docker/push.sh
@@ -0,0 +1,2 @@
+docker push pipelineai/serving-gpu:1.5.0
+docker push pipelineai/serving-cpu:1.5.0
